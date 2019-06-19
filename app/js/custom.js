0aeed310ca690e0d2e3509ae988552ac2bd75ed5
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-06-19 12:51:48 +0300

    add toggle btn

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,3 +1,10 @@
 "use strict";
 
-window.onload = function () {};
+window.onload = function () {
+  var toggleBtn = document.getElementById("main-nav__btn-toggle");
+  toggleBtn.addEventListener("click", showMenu);
+
+  function showMenu() {
+    toggleBtn.classList.toggle("main-nav__btn--open");
+  }
+};
--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -1,1 +1,9 @@
-window.onload = () => {};
+window.onload = () => {
+  const toggleBtn = document.getElementById("main-nav__btn-toggle");
+
+  toggleBtn.addEventListener("click", showMenu);
+
+  function showMenu() {
+    toggleBtn.classList.toggle("main-nav__btn--open");
+  }
+};
