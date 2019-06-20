3a1740a431aa115adc522d82f38eec363a2a922c
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-06-20 21:11:35 +0300

    start faucet block

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,10 +1,25 @@
 "use strict";
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
 window.onload = function () {
   var toggleBtn = document.getElementById("main-nav__btn-toggle");
+  var nainNavList = document.getElementById("main-nav__list");
+  var mainNavLink = document.querySelectorAll(".main-nav__link");
   toggleBtn.addEventListener("click", showMenu);
+
+  _toConsumableArray(mainNavLink).forEach(function (el) {
+    el.addEventListener("click", showMenu);
+  });
 
   function showMenu() {
     toggleBtn.classList.toggle("main-nav__btn--open");
+    nainNavList.classList.toggle("main-nav__list--open");
   }
 };
--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -1,9 +1,16 @@
 window.onload = () => {
   const toggleBtn = document.getElementById("main-nav__btn-toggle");
+  const nainNavList = document.getElementById("main-nav__list");
+  const mainNavLink = document.querySelectorAll(".main-nav__link");
 
   toggleBtn.addEventListener("click", showMenu);
 
+  [...mainNavLink].forEach(el => {
+    el.addEventListener("click", showMenu);
+  });
+
   function showMenu() {
     toggleBtn.classList.toggle("main-nav__btn--open");
+    nainNavList.classList.toggle("main-nav__list--open");
   }
 };
