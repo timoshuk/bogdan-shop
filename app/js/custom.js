be5c2eee69e7e9c2988306ccbc5af555b0727a26
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-06-30 10:40:05 +0300

    Finish page all

--- app/js/custom.js
+++ app/js/custom.js
@@ -13,21 +13,54 @@ window.onload = function () {
   var nainNavList = document.getElementById("main-nav__list");
   var mainNavLink = document.querySelectorAll(".main-nav__link");
   var imagesLazy = document.querySelectorAll("img[data-src]");
+  var header = document.getElementById("header");
+  var headerHeight = header.offsetHeight;
   imgLazy(imagesLazy);
   toggleBtn.addEventListener("click", showMenu);
 
   _toConsumableArray(mainNavLink).forEach(function (el) {
-    el.addEventListener("click", showMenu);
-  });
+    el.addEventListener("click", function (event) {
+      event.preventDefault();
+      scrollToEl(this);
+      showMenu();
+    });
+  }); // Scroll to block #id
+
+
+  function scrollToEl(el) {
+    var attr = el.getAttribute("href");
+    var scr = document.querySelector(attr).offsetTop - headerHeight;
+    scr > 0 ? window.scrollTo(0, scr) : window.scrollTo(0, 0);
+  } // Show hide main-naw. Change view main-nav__btn
+
 
   function showMenu() {
     toggleBtn.classList.toggle("main-nav__btn--open");
     nainNavList.classList.toggle("main-nav__list--open");
-  }
+  } // Lazy load img
+
 
   function imgLazy(imgs) {
     _toConsumableArray(imgs).forEach(function (itemImg) {
       itemImg.setAttribute("src", itemImg.getAttribute("data-src"));
     });
+  } //get header heit on resize window
+
+
+  window.onresize = function () {
+    headerHeight = header.offsetHeight;
+  };
+
+  window.onscroll = function () {
+    winScrollChange();
+  }; // add class heder--scroll to header
+
+
+  function winScrollChange() {
+    if (window.scrollY > headerHeight) {
+      header.classList.add("header--scroll");
+    } else {
+      header.classList.remove("header--scroll");
+    }
   }
 };
--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -3,21 +3,53 @@ window.onload = () => {
   const nainNavList = document.getElementById("main-nav__list");
   const mainNavLink = document.querySelectorAll(".main-nav__link");
   const imagesLazy = document.querySelectorAll("img[data-src]");
+  const header = document.getElementById("header");
+  let headerHeight = header.offsetHeight;
   imgLazy(imagesLazy);
   toggleBtn.addEventListener("click", showMenu);
 
   [...mainNavLink].forEach(el => {
-    el.addEventListener("click", showMenu);
+    el.addEventListener("click", function(event) {
+      event.preventDefault();
+      scrollToEl(this);
+      showMenu();
+    });
   });
 
+  // Scroll to block #id
+  function scrollToEl(el) {
+    const attr = el.getAttribute("href");
+    let scr = document.querySelector(attr).offsetTop - headerHeight;
+    scr > 0 ? window.scrollTo(0, scr) : window.scrollTo(0, 0);
+  }
+
+  // Show hide main-naw. Change view main-nav__btn
   function showMenu() {
     toggleBtn.classList.toggle("main-nav__btn--open");
     nainNavList.classList.toggle("main-nav__list--open");
   }
 
+  // Lazy load img
   function imgLazy(imgs) {
     [...imgs].forEach(itemImg => {
       itemImg.setAttribute("src", itemImg.getAttribute("data-src"));
     });
+  }
+
+  //get header heit on resize window
+  window.onresize = function() {
+    headerHeight = header.offsetHeight;
+  };
+  window.onscroll = function() {
+    winScrollChange();
+  };
+
+  // add class heder--scroll to header
+  function winScrollChange() {
+    if (window.scrollY > headerHeight) {
+      header.classList.add("header--scroll");
+    } else {
+      header.classList.remove("header--scroll");
+    }
   }
 };
